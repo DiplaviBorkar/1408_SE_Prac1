cc3e35fc929ecaf629fb87d26405d6b2404f7578
DiplaviBorkar <borkardiplavid@gmail.com>
2015-12-21 23:15:29 +0530

    commit2

--- Prac2/src/Queues/QueueArrays.java
+++ Prac2/src/Queues/QueueArrays.java
@@ -1,18 +1,51 @@
 package Queues;
 
 public class QueueArrays {
-	int size;
-	int data;
-	int front=0,rear;
+	int size,i;
+	int front,rear;
+	int QArray[];
 	
-	public QueueArrays(){
-		this.data=data;
-
+	public QueueArrays(int size){
+		this.size=size;
+		this.QArray=new int[size];
+		this.front=this.rear=-1;
 	}
 	
+	public boolean IsFull(){
+		if(rear==(size-1))
+		return true;
+		else
+			return false;
+	}
 	
 	public void enqueue(int value){
 		
+		if(IsFull()==true){
+			System.out.println("The queue is full");
+		}
+		else
+			
+			if(front==-1){
+				front=front+1;
+			
+			this.QArray[rear+1]=value;
+			System.out.println("Element enqueued"+QArray[rear+1]);
+	}
+	
+	}
+	
+	
+	public void dequeue(){
 		
+		System.out.println("Element dequeued"+QArray[front]);
+		front=front+1;
+			
+	}
+	
+	public void display(){
+		System.out.println("Queue Elements are:");
+		for(i=front;i<rear;i++){
+			System.out.println("\n"+QArray[i]);
+		}
 	}
 }
--- Prac2/src/Queues/QuieueClient.java
+++ Prac2/src/Queues/QuieueClient.java
@@ -3,8 +3,11 @@ package Queues;
 public class QuieueClient {
 
 	public static void main(String[] args) {
-		
-
+		QueueArrays queueArrays=new QueueArrays(5);
+		queueArrays.enqueue(100);
+		queueArrays.dequeue();
+		queueArrays.enqueue(1000);
+		queueArrays.display();
 	}
 
 }
